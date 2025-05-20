49074bf6cbe2cfce9f2bb44c0cde39a75cd8123a
flaxytop <sashabropio@gmail.com>
2025-05-20 17:31:12 +0300

    delete d

--- ShopFlowers02/Flowers/DTOs/RegisterDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Flowers.DTOs
-{
-    public class RegisterDto
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public string? Role { get; set; }
-    }
-}
